9b7b2f4d42037323db3e2b988f8211ed149a5cf8
yeonjy <81320703+yeonjy@users.noreply.github.com>
2021-07-28 14:10:05 +0900

    Update condition.c

--- condition.c
+++ condition.c
@@ -114,6 +114,77 @@ int main_condition(void)
 	default:printf("몰라요\n"); break;           // 다 아닐 경우
 	// case 특징: 만약 i = 1 이면 case 0 스킵, case 1 출력 그 이후 코드 조건은 확인하지 않고 무조건 출력  => break; 써서 해당 반복 탈출하도록 함
 	}
+	
+	int age = 19;
+	switch (age)
+	{
+	case 8:
+	case 9:
+	case 10:
+	case 11:
+	case 12:                                          // 같은 텍스트를 여러번 쓰지 않기 위해 break를 쓰지 않고 같은 텍스트의 마지막 코드에만 해당 텍스트 작성
+	case 13:printf("초등학생입니다.\n"); break;
+	case 14:
+	case 15:
+	case 16:printf("중학생입니다.\n"); break;
+	case 17:
+	case 18:
+	case 19:printf("고등학생입니다.\n"); break;
+	default: printf("학생이 아닌가봐요.\n"); break;
+	}
+	if (age >= 8 && age <= 13)          // &&의 뜻: 양 옆 변의 조건이 모두 만족할 때 (age >=8 이면서 age <= 13 일 때)
+	{
+		printf("초등학생입니다\n");
+	}
+	else if (age >= 14 && age <= 16)
+	{
+		printf("중학생입니다.\n");
+	}
+	else if (age >= 17 && age <= 19)
+	{
+		printf("고등학생입니다.\n");
+	}
+	else
+		printf("학생이 아닌가봐요");
+	
+	
+	// Up and Down
+	srand(time(NULL));
+	int num = rand() % 100 + 1;     // 1 ~ 100 까지의 숫자
+	printf("숫자 : %d\n", num);
+	int answer = 0; // 정답
+	int chance = 5; // 기회
+	while (chance > 0)
+	// while (1)           // 1 : 참, 0 : 거짓  => 무한루프로 사용 가능
+	{
+		printf("남은 기회 %d번 \n", chance--);
+		printf("숫자를 맞혀보세요 (1~100) : ");
+		scanf_s("%d", &answer);
+
+		if (answer > num)
+		{
+			printf("DOWN ↓ \n\n");             // ㅁ 입력 후 '한자' 버튼 누르면 기호 선택할 수 있음
+		}
+		else if (answer < num)
+		{
+			printf("UP ↑ \n\n");
+		}
+		else if (answer == num)
+		{
+			printf("정답입니다! \n\n");
+			break;
+		}
+		else
+		{
+			printf("알 수 없는 오류가 발생했어요\n\n");
+		}
+
+		if (chance == 0)
+		{
+			printf("모든 기회를 다 사용하셨네요. 아쉽게 실패하셨습니다.\n");
+			break;
+		}
+	}
 
 	return 0;
 }
